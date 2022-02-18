195cab62a2ceca4c493651adec7d35c0c3a89eff
priyanka <priyanka.mukhi@3SCSOLUTION.LAN>
2022-02-18 15:50:32 +0530

    files added

---
+++ calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            Console.WriteLine("enter first input");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second input");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            result = num1 + num2;
+            Console.WriteLine("the result is {0}",result);
+            result = num1 - num2;
+            Console.WriteLine("the result is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("the result is {0}", result);
+
+
+
+        }
+        
+    }
+}
